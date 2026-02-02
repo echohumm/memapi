3925c695b602373c76616c2f7e20e214889fcf9a
echohumm <echo0humm@gmail.com>
2026-02-02 14:40:41 -0700

    add ci tests, guaranteed alignment handling for stackalloc

--- src/ffi/c/calloca.c
+++ src/ffi/c/calloca.c
@@ -1,15 +1,47 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 #include <string.h>
 
-#ifdef _MSC_VER
-# include <malloc.h>
-#endif
-#ifndef _MSC_VER
-# include <alloca.h>
+#if defined(_MSC_VER)
+  #include <malloc.h>
+#elif !defined(__MINGW64__)
+  #include <alloca.h>
 #endif
 
-// TODO: try to find some way to avoid stack overflow if too large
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201112L
+  /* C11: use max_align_t */
+  #include <stdalign.h>
+  #include <stddef.h>
+  #define GUARANTEED_ALIGN alignof(max_align_t)
+
+#elif defined(__GNUC__) || defined(__clang__)
+  /* GCC/Clang: try to use max_align_t if available, otherwise pick a wide alignment */
+  #include <stddef.h>
+  #if defined(__SIZEOF_MAX_ALIGN_T__)
+    /* some targets expose max_align_t even in non-C11 modes */
+    #define GUARANTEED_ALIGN __alignof__(max_align_t)
+  #elif defined(__MINGW64__) || defined(__x86_64__) || defined(_M_X64) || defined(_M_AMD64)
+    /* x86_64 targets: follow the 16-byte Windows / System V ABI */
+    #define GUARANTEED_ALIGN 16
+  #else
+    /* conservative fallback, long double has large alignment */
+    #define GUARANTEED_ALIGN __alignof__(long double)
+  #endif
+
+#elif defined(_MSC_VER)
+  /* MSVC: x64 = 16, x86 = 8 (conservative) */
+  #if defined(_M_X64) || defined(_M_AMD64)
+    #define GUARANTEED_ALIGN 16
+  #else
+    #define GUARANTEED_ALIGN 8
+  #endif
+
+#else
+  #define GUARANTEED_ALIGN 16
+#endif
+
+/* TODO: try to find some way to avoid stack overflow if too large */
 void c_alloca(
     const size_t size,
     const size_t align,
@@ -22,8 +54,16 @@ void c_alloca(
 
     if (size == 0) {
         ptr = (uint8_t*)align;
+    } else if (align <= GUARANTEED_ALIGN) {
+        #ifdef _MSC_VER
+            ptr = _alloca(size);
+        #else
+            ptr = alloca(size);
+        #endif
+        if (zero) {
+            memset(ptr, 0, size);
+        }
     } else {
-        // TODO: ideally we ignore user's alignment request if the platform's guaranteed align is >=
         size_t m1 = align - 1;
         size_t alloc_size = size + m1;
         #ifdef _MSC_VER
